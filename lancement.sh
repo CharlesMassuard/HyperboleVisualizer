73b3708abd3204881ece611d2f289ee71e5bb331
CharlesMassuard <charles.massuard@etu.univ-orleans.fr>
2024-10-01 14:50:56 +0200

    fermeture des terminaux après execution

--- lancement.sh
+++ lancement.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "python3 ./serveur.py; exec bash"
-gnome-terminal -- bash -c "python3 ./client.py; exec bash"
+gnome-terminal -- bash -c "python3 ./serveur.py"
+gnome-terminal -- bash -c "python3 ./client.py"
