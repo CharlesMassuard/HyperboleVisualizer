938574a32584bb9f29a7a58d90a9b5382df38bee
noa-menjikoff-guillot <o22203433@or-iut-i003-f31.iut45.univ-orleans.fr>
2024-11-20 15:26:40 +0100

    initialisation de la page home

---
+++ hyperbole_visualiser/src/Components/home.js
@@ -0,0 +1,12 @@
+import '../CSS/home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Hyperbole Visualiser</h1>
+            <p>Enter a sentence and see the hyperboles in it!</p>
+        </div>
+    );
+}
+
+export default Home;
